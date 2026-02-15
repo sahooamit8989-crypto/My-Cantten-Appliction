49d3f714d9f37382245631d312f4231c02b521fc
Amit Sahoo <sahooamit8989@gmail.com>
2026-02-15 13:03:24 +0530

    Update WhatsAppButton.jsx

--- frontend/src/components/WhatsAppButton/WhatsAppButton.jsx
+++ frontend/src/components/WhatsAppButton/WhatsAppButton.jsx
@@ -5,7 +5,7 @@ import "./WhatsAppButton.css"; // import the CSS file
 export default function WhatsAppButton() {
   return (
     <a
-      href="https://wa.me/7849047603?text=Greetings%2C%20I%20am%20interested%20in%20learning%20Odissi%20and%20would%20like%20more%20details%20about%20classes%20and%20admissions."
+      href="https://wa.me/7849047603"
       target="_blank"
       rel="noopener noreferrer"
       title="Chat with us on WhatsApp"
